3774ffb2774ac3f9c37cd2c81e92fa94066d15e8
Johnny Robeson <johnny@localmomentum.net>
2014-07-28 05:39:21 -0400

    add our extension and configuration classes
    This adds configuration for all the extendable methods from
    the controller class.
    
    Currently this only supports the yaml file format.
    
    You can view all the configuration options by reading the Configuration
    class or running  `app/console config:dump-reference c33s_static_pages`

--- c33sStaticPageContentBundle.php
+++ c33sStaticPageContentBundle.php
@@ -1,18 +1,21 @@
 <?php
-
-/*
-* This file is part of the c33s\StaticPageContentBundle.
-*
-* (c) consistency <office@consistency.at>
-*
-* For the full copyright and license information, please view the LICENSE
-* file that was distributed with this source code.
-*/
-
+/**
+ * This file is part of the c33s\StaticPageContentBundle.
+ *
+ * (c) consistency <office@consistency.at>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace c33s\StaticPageContentBundle;
 
+use c33s\StaticPageContentBundle\DependencyInjection\StaticPagesExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class c33sStaticPageContentBundle extends Bundle
 {
+    public function getContainerExtension()
+    {
+        return new StaticPagesExtension();
+    }
 }
